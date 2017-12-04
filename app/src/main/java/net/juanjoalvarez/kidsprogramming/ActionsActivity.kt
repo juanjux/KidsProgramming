7ffef905fcffd95b221484da6be87edfe9bcc2c8
Juanjo Alvarez <juanjo@juanjoalvarez.net>
2017-12-04 16:10:14 +0100

    Basic Activities wired.
    Signed-off-by: Juanjo Alvarez <juanjo@juanjoalvarez.net>

---
+++ app/src/main/java/net/juanjoalvarez/kidsprogramming/ActionsActivity.kt
@@ -0,0 +1,13 @@
+package net.juanjoalvarez.kidsprogramming
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class ActionsActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_actions)
+    }
+
+}
--- app/src/main/java/net/juanjoalvarez/kidsprogramming/CodeActivity.kt
+++ app/src/main/java/net/juanjoalvarez/kidsprogramming/CodeActivity.kt
@@ -1,7 +1,9 @@
 package net.juanjoalvarez.kidsprogramming
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 
 class CodeActivity : AppCompatActivity() {
 
@@ -10,5 +12,24 @@ class CodeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_code)
+
+        val btn_activator: Button = findViewById<Button>(R.id.button_activator)
+        val btn_condition: Button = findViewById<Button>(R.id.button_condition)
+        val btn_actions: Button = findViewById<Button>(R.id.button_actions)
+
+        btn_activator.setOnClickListener {
+            val activatorIntent = Intent(this, ActivatorActivity::class.java)
+            startActivity(activatorIntent)
+        }
+
+        btn_condition.setOnClickListener {
+            val conditionIntent = Intent(this, ConditionActivity::class.java)
+            startActivity(conditionIntent)
+        }
+
+        btn_actions.setOnClickListener {
+            val actionsIntent = Intent(this, ActionsActivity::class.java)
+            startActivity(actionsIntent)
+        }
     }
 }
